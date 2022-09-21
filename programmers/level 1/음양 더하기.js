f0ee3f464ce8ec4f7fc0d0b6f4a9a6fe6835af90
rami0617 <aarmakrr@hanmail.net>
2022-09-21 22:17:16 +0900

    2022/09/21

---
+++ programmers/level 1/음양 더하기.js
@@ -0,0 +1,20 @@
+function solution(absolutes, signs) {
+  //for문
+  // let answer = 0;
+
+  // for(let i = 0; i < absolutes.length; i++) {
+  //     if(signs[i]) {
+  //         answer += absolutes[i];
+  //     } else {
+  //         answer -= absolutes[i];
+  //     }
+  // }
+  // return answer;
+
+  //reduce
+  return absolutes.reduce(
+    (prev, current, index) =>
+      signs[index] === true ? prev + current : prev - current,
+    0
+  );
+}
