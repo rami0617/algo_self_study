f18853401ccfc56b56d67f098ce0f22b29e32ee6
최아람 <archoi@autocrypt.io>
2023-10-18 22:17:08 +0900

    2023/10/18

---
+++ programmers/level 1/문자열 나누기.js
@@ -0,0 +1,36 @@
+function solution(s) {
+  if (s.length === 1) {
+    return 1;
+  }
+
+  let centerString = s[0];
+  let count = 0;
+  // let diffrenceCount = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const result = getDiffirentCount(s.slice(i, s.length), s[i]);
+    i += result - 1;
+
+    count++;
+  }
+  return count;
+}
+
+function getDiffirentCount(s, centerString) {
+  let diffrenceCount = 0;
+  let sameCount = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (centerString === s[i]) {
+      sameCount++;
+    } else if (centerString !== s[i]) {
+      diffrenceCount++;
+    }
+
+    if (sameCount === diffrenceCount) {
+      break;
+    }
+  }
+
+  return diffrenceCount + sameCount;
+}
